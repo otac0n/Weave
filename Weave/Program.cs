4ab53d9994616c797f8ac3de3fed89156dc20790
John Gietzen <otac0n@gietzen.us>
2013-03-02 22:25:50 -0800

    Created the Weave project.

---
+++ Weave/Program.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="(none)">
+//   Copyright © 2013 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Weave
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Weave
+=====
+
+Weave is a text layout engine for .NET that is all about attention to detail.
