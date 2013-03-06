33794ca921b6ca2e3608b2532da989d05e004789
John Gietzen <otac0n@gietzen.us>
2013-03-06 08:13:36 -0800

    Implemented the IfTag class and parser rules.

---
+++ Weave/Expressions/Branch.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Branch.cs" company="(none)">
+//   Copyright © 2013 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Weave.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Represents one branch of an <see cref="IfTag"/>.
+    /// </summary>
+    public class Branch
+    {
+        private readonly IList<Element> body;
+        private readonly string expression;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Branch"/> class.
+        /// </summary>
+        /// <param name="expression">The code expression that determines whether this branch will execute. Null, if this is the default branch.</param>
+        /// <param name="body">The body of the branch.</param>
+        public Branch(string expression, IList<Element> body)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException("body");
+            }
+
+            this.body = body.ToList().AsReadOnly();
+            this.expression = expression;
+        }
+    }
+}
--- Weave/Expressions/IfTag.cs
+++ Weave/Expressions/IfTag.cs
@@ -8,10 +8,29 @@
 
 namespace Weave.Expressions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     /// <summary>
     /// Represents a conditional section in the <see cref="Template"/>.
     /// </summary>
     public class IfTag : Element
     {
+        private readonly IList<Branch> branches;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IfTag"/> class.
+        /// </summary>
+        /// <param name="branches">The branches that make up this tag.</param>
+        public IfTag(IEnumerable<Branch> branches)
+        {
+            if (branches == null)
+            {
+                throw new ArgumentNullException("branches");
+            }
+
+            this.branches = branches.ToList().AsReadOnly();
+        }
     }
 }
