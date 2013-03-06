e0fd7841f1f1202cff1fab084fbf4083a3bafd94
John Gietzen <otac0n@gietzen.us>
2013-03-05 23:57:43 -0800

    Implemented the EchoTag class and parse rule.

--- Weave/Expressions/EchoTag.cs
+++ Weave/Expressions/EchoTag.cs
@@ -8,10 +8,30 @@
 
 namespace Weave.Expressions
 {
+    using System;
+
     /// <summary>
     /// Represents a computed section in the <see cref="Template"/>.
     /// </summary>
     public class EchoTag : Element
     {
+        private readonly bool encoded;
+        private readonly string expression;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EchoTag"/> class.
+        /// </summary>
+        /// <param name="expression">The code expression that will be used to compute the text to render.</param>
+        /// <param name="encoded">True, if the output is to be encoded; false, otherwise.</param>
+        public EchoTag(string expression, bool encoded)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            this.expression = expression;
+            this.encoded = encoded;
+        }
     }
 }
