0c223c780b3b89509b140281f774e2a6b164425f
John Gietzen <otac0n@gietzen.us>
2013-03-06 00:09:25 -0800

    Implemente the EachTag class and parse rule.

--- Weave/Expressions/EachTag.cs
+++ Weave/Expressions/EachTag.cs
@@ -8,10 +8,40 @@
 
 namespace Weave.Expressions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     /// <summary>
     /// Represents a repeated section of the <see cref="Template"/>.
     /// </summary>
     public class EachTag : Element
     {
+        private readonly IList<Element> body;
+        private readonly string expression;
+        private readonly IList<Element> noneBody;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EachTag"/> class.
+        /// </summary>
+        /// <param name="expression">The code expression that describes the iteration subject.</param>
+        /// <param name="body">The body of the loop.</param>
+        /// <param name="noneBody">An optional body that is rendered when the iteration subject is empty.</param>
+        public EachTag(string expression, IList<Element> body, IList<Element> noneBody)
+        {
+            if (expression == null)
+            {
+                throw new ArgumentNullException("expression");
+            }
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("body");
+            }
+
+            this.body = body.ToList().AsReadOnly();
+            this.expression = expression;
+            this.noneBody = noneBody == null ? null : noneBody.ToList().AsReadOnly();
+        }
     }
 }
