e95fe3726b6e1c5666b301f4f0a5331f21ced21b
John Gietzen <otac0n@gietzen.us>
2013-03-09 10:42:51 -0800

    StyleCop

--- Weave/Compiler/Templates.cs
+++ Weave/Compiler/Templates.cs
@@ -1,4 +1,12 @@
-﻿namespace Weave.Compiler
+﻿// -----------------------------------------------------------------------
+// <copyright file="Templates.cs" company="(none)">
+//   Copyright © 2013 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.txt for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Weave.Compiler
 {
     using System.Collections.Generic;
     using System.Globalization;
