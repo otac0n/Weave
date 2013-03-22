c2b01c4cde047e34465ea44f795603ee7d10df24
John Gietzen <otac0n@gietzen.us>
2013-03-21 19:12:12 -0700

    Added comment for NewLineElement.

--- Weave/Expressions/NewLineElement.cs
+++ Weave/Expressions/NewLineElement.cs
@@ -8,7 +8,10 @@
 
 namespace Weave.Expressions
 {
-    internal class NewLineElement : Element
+    /// <summary>
+    /// Represents the end of a line.
+    /// </summary>
+    public class NewLineElement : Element
     {
     }
 }
